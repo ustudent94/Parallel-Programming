b0165367e8a0c2cda90846d2af7d5883c2f79722
unknown <cory.s.christensen@hotmail.com>
2020-02-11 15:41:30 -0700

    Framework is set compile errors are removed. Requires debugging

--- Assn5/mbrot.cpp
+++ Assn5/mbrot.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <mpi.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+#define MCW MPI_COMM_WORLD
 using namespace std;
 
 struct Complex {
@@ -53,48 +57,101 @@ int mbrotIters(Complex c, int maxIters){
 
 int main(int argc, char **argv){
 
-    Complex c1,c2,c3;
+    int rank, size, tag;
+    //int data[64];
+    MPI_Status mystatus;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MCW, &rank);
+    MPI_Comm_size(MCW, &size);
+
+
     Complex c;
-    ofstream fout;
-    fout.open("image.ppm");
-    //params(file, c1.r,c1.i,c2.r)
+    const int DIM = 512;
+    Complex row[DIM];
+    int iters[DIM];
+    int finalIters[DIM][DIM];
+
+    if(!rank) {
+        Complex c1, c2, c3;
+
+        ofstream fout;
+        fout.open("image.ppm");
+        //params(file, c1.r,c1.i,c2.r)
 //    c1.r=0.5;
 //    c1.i=0.39;
 //    c2.r=-2;
 //    c2.i=-2;
-    c1.r=atof(argv[1]);
-    c1.i=atof(argv[2]);
-    c2.r=atof(argv[3]);
-    c2.i=c1.i + (c2.r-c1.r);
+        c1.r = atof(argv[1]);
+        c1.i = atof(argv[2]);
+        c2.r = atof(argv[3]);
+        c2.i = c1.i + (c2.r - c1.r);
 
-    c3=c1+c2;
-    cout << c3.r << " + " << c3.i <<"i"<<endl;
+        c3 = c1 + c2;
+        cout << c3.r << " + " << c3.i << "i" << endl;
 
-    c3=c1*c2;
-    cout << c3.r << " + " << c3.i <<"i"<<endl;
+        c3 = c1 * c2;
+        cout << c3.r << " + " << c3.i << "i" << endl;
 
-    int DIM = 500;
-    fout<<"P3"<<endl;
-    fout<<DIM<<" "<<DIM<<endl;
-    fout<<255<<endl;
-
-    for(int j=0;j<DIM;++j){
-        for(int i=0;i<DIM;++i){
-            c.r = (i*(c1.r-c2.r)/DIM)+c2.r;
-            c.i = (j*(c1.i-c2.i)/DIM)+c2.i;
-
-            int iters = mbrotIters(c,255);
-            fout << rcolor(iters)<<" ";
-            fout << gcolor(iters)<<" ";
-            fout << bcolor(iters)<<" ";
-
+        //send a row to each process initially
+        fout << "P3" << endl;
+        fout << DIM << " " << DIM << endl;
+        fout << 255 << endl;
+        for (int j = 0; j < size; ++j) {
+            for (int i = 0; i < DIM; ++i) {
+                c.r = (i * (c1.r - c2.r) / DIM) + c2.r;
+                c.i = (j * (c1.i - c2.i) / DIM) + c2.i;
+                row[i] = c;
+            }
+            MPI_Send(row,DIM,MPI_INT,0,j,MCW);
         }
-        fout << endl;
+
+        for (int j = size +1; j < DIM; ++j) {
+            for (int i = 0; i < DIM; ++i) {
+                c.r = (i * (c1.r - c2.r) / DIM) + c2.r;
+                c.i = (j * (c1.i - c2.i) / DIM) + c2.i;
+                row[i] = c;
+            }
+            //recieve row of iters into finalItes[tag]
+            MPI_Probe(MPI_ANY_SOURCE,0,MCW,&mystatus);
+            tag = mystatus.MPI_TAG;
+            MPI_Recv(iters,DIM,MPI_INT,MPI_ANY_SOURCE,0,MCW,&mystatus);
+            
+            for(int k = 0; k < DIM; ++k) {
+                finalIters[tag][k] = iters[k];
+            }
+            //send row of complex numbers
+            MPI_Send(row,DIM,MPI_INT,0,j,MCW);
+        }
+
+
+
+
+        //output file
+        for(int j = 0; j <DIM; ++j) {
+            for (int i = 0; i < DIM; ++i) {
+                fout << rcolor(finalIters[j][i]) << " ";
+                fout << gcolor(finalIters[j][i]) << " ";
+                fout << bcolor(finalIters[j][i]) << " ";
+            }
+            fout << endl;
+        }
+        fout.close();
     }
-    
 
-    fout.close();
+    //other processes
+    else{
+        //int iters;
+        MPI_Probe(MPI_ANY_SOURCE,0,MCW,&mystatus);
+        tag = mystatus.MPI_TAG;
+        MPI_Recv(row,DIM,MPI_INT,MPI_ANY_SOURCE,0,MCW,&mystatus);
+        for(int i = 0; i < DIM; ++i){
+            iters[i] = mbrotIters(row[i],255);
+        }
 
+        //MPI_Send(iters,DIM,MPI_INT,0,tag,MCW);
+    }
+
+    MPI_Finalize();
     return 0;
 }
 
