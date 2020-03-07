f8b9ddc8e1dc24fb63452221cdbf5edbe32531de
Cory Christensen <cory.s.christensen@hotmail.com>
2020-03-07 11:07:39 -0700

    I beleive the program is working. Need to change to random max but otherwise done

--- Assn7/Assn7.cpp
+++ Assn7/Assn7.cpp
@@ -11,8 +11,7 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-    int rank, size, process,source;
-    //todo: change tasks to array
+    int rank, size, process,source, tag, flag;
     int tasks;
     MPI_Status mystatus;
     MPI_Init(&argc, &argv);
@@ -26,122 +25,124 @@ int main(int argc, char **argv) {
     vector<int> queue;
     int taskNum = 0;
     //todo: change to random amount of work
-    int maxTask = 1024;
+    int maxTask = 100;
 //    int maxTask = (rand() % 1024) + 1024
-    bool processWhite = true;
+    bool processWhite = true;\
+    bool sendToken = false;
     int token =0;
     //token 0 = no token, 1 = white, 2 = black
     int numIter;
 
 
 
-
-
+//    taskNum = maxTask;
+    ///kickoff
     if(!rank){
         //this is to initiate process only
         tasks = taskNum;
+//        tasks = -2;
         cout << "Initializing work at process 0 starting with task " << taskNum << endl;
         MPI_Send(&tasks, 1, MPI_INT, 0, 0, MCW);
-    }else{
-        while(1){
+//        MPI_Send(&tasks, 1, MPI_INT, 1, 1, MCW);
+        token = 0;
+    }
+
+
+    while(1){
 //            MPI_Probe(MPI_ANY_SOURCE,MPI_ANY_TAG,MCW,&mystatus);
-            MPI_Iprobe(MPI_ANY_SOURCE,MPI_ANY_TAG,MCW,&flag,mystatus);
-            if(flag) {
-                MPI_Get_count(&mystatus, MPI_INT, &count);
-                source = mystatus.MPI_SOURCE;
-                tag = mystatus.MPI_TAG;
-            }
+        MPI_Iprobe(MPI_ANY_SOURCE,MPI_ANY_TAG,MCW,&flag,&mystatus);
+        if(flag) {
+            //MPI_Get_count(&mystatus, MPI_INT, &count);
+            source = mystatus.MPI_SOURCE;
+            tag = mystatus.MPI_TAG;
+        }
 
-            if(tag == 1 && flag){
-                MPI_Recv(&token,1,MPI_INT,MPI_ANY_SOURCE,1,MCW,&mystatus);
-                cout << rank << " received token " << token << " from " << source << endl;
-                ///termination logic
-//            if(count == 1) {
-                if (token == -2 && rank == 0) {
-                    //send out termination message
-                    token = -1;
-                    for (int i = 1; i < size; i++) {
-                        cout << "Terminating process " << i;
-                        MPI_Send(&token, 1, MPI_INT, i, 1, MCW);
-                    }
-                    break;
+        if(tag == 1 && flag){
+            MPI_Recv(&token,1,MPI_INT,MPI_ANY_SOURCE,1,MCW,&mystatus);
+            cout << rank << " received token " << token << " from " << source << endl;
+            ///termination logic
+            if (token == -2 && rank == 0) {
+                //send out termination message
+                token = -1;
+                for (int i = 1; i < size; i++) {
+                    cout << "Terminating process " << i << endl;
+                    MPI_Send(&token, 1, MPI_INT, i, 1, MCW);
                 }
-                if (token == -1 && rank != 0) {
-                    cout << rank << " terminated" << endl;
-                    break;
+                break;
+            }
+            if(token == -3 && rank == 0){
+                token = -2;
+            }
+            if (token == -1 && rank != 0) {
+                cout << rank << " terminated" << endl;
+                break;
+            }
+        }
+
+        ///work logic
+
+        //recieve message if message waiting
+        if(tag == 0 && flag) {
+            MPI_Recv(&tasks, 1, MPI_INT, MPI_ANY_SOURCE, 0, MCW, &mystatus);
+//            cout << rank << " received " << tasks << " task from " << source << endl;
+            queue.push_back(tasks);
+        }
+
+
+        //send off excess work
+        if(queue.size() >16){
+
+            for(int i = 0; i < 2; i++){
+                process = rand() %size;
+                tasks = queue.back();
+                queue.pop_back();
+
+                if(process < rank){
+                    processWhite = false;
                 }
+//                cout << rank << " has " << queue.size() << " elements in queue. Sending to process " <<process << endl;
+                MPI_Send(&tasks, 1, MPI_INT, process, 0, MCW);
             }
+        }
 
-            ///work logic
-
-            //recieve message if message waiting
-            if(tag == 0 && flag) {
-                MPI_Recv(&tasks, count, MPI_INT, MPI_ANY_SOURCE, 0, MCW, &mystatus);
-                cout << rank << " received " << tasks << " task from " << source << endl;
-//todo: recieve whole array
-                //receive work into queue
-//                for(int i = 0; i < count;i++){
-//                    queue.pushb
-//                }
-                queue.push_back(tasks);
+        //do work
+        if(queue.size() > 0){
+            int task = queue.back();
+            queue.pop_back();
+            numIter = task * task;
+            for(int i = 0;  i < numIter; i++){
+                task++;
             }
+        }
 
-
-            //send off excess work
-            if(queue.size >16){
-
-                for(int i = 0; i < 2; i++){
-                    process = rand(rank) %size;
-                    count = (rand(i)%2) +1;
-                    //todo: add for loop to put into tasks
-                    tasks = queue.back();
-                    queue.pop_back();
-
-                    if(process < rank){
-                        processWhite = false;
-                    }
-                    cout << rank << " has " << queue.size() << " elements in queue. Sending to process " <<process << endl;
-                    //todo: change to count
-                    MPI_Send(&tasks, 1, MPI_INT, process, 0, MCW);
-                }
+        //add new tasks
+        int numNew = (rand()%2) +1;
+        int counter = 0;
+        while (taskNum < maxTask && counter < numNew){
+            taskNum++;
+//            cout << rank << " added " << taskNum << " to the queue " << endl;
+            queue.push_back(taskNum);
+            counter++;
+            if(taskNum>= maxTask){
+                cout << rank << " is done generating tasks " << endl;
             }
-
-            //do work
-            if(queue.size() > 0){
-                int task = queue.back();
-                numIter = task * task;
-                for(int i = 0;  i < numIter; i++){
-                    task++;
-                }
-            }
-
-            //add new tasks
-            int numNew = (rand(rank)%2) +1;
-            int counter = 0;
-            while (taskNum < maxTask && counter < numNew){
-                taskNum++;
-                queue.add(taskNum)
-                counter++;
-            }
-
-            if(taskNum >= maxTask && rank ==0){
+            if(taskNum>= maxTask && rank == 0){
                 cout << rank << " sending token around" << endl;
                 token = -2;
             }
+        }
 
+        ///send token
+        if(queue.size() == 0 && taskNum >= maxTask && token!=0){
+            process = (rank +1) %size;
 
-            ///send token
-            if(queue.size() == 0 && token!=0){
-                process = rank +1 %size;
-
-                if(!processWhite){
-                    token = -3;
-                    processWhite = true;
-                }
-                cout << rank << " sending token " << token==-2? "white":"black" << " to " << process << endl;
-                MPI_Send(&token, 1, MPI_INT, process, 1, MCW);
-                token = 0;
+            if(!processWhite){
+                token = -3;
+                processWhite = true;
             }
+            cout << rank << " sending token " << token << " to " << process << endl;
+            MPI_Send(&token, 1, MPI_INT, process, 1, MCW);
+            token = 0;
         }
     }
 
